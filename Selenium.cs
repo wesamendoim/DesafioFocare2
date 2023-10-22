43de0d7b182567ba5b63ebad791758fb1315badf
wesamendoim <wesleyvbnardino@hotmail.com>
2023-10-22 11:18:12 -0300

    Add project files.

---
+++ Selenium.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Chrome;
+
+namespace ConsoleApp1
+{
+    public class Selenium
+    {
+        public void BrowserChrome(string url)
+        {
+            ChromeDriver objChrome = new ChromeDriver();
+
+            objChrome.Url = url;
+
+
+        }
+
+    }
+}
