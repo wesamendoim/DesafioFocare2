c4add60cf1eaf816171c96e699378d0dd4d63c5a
wesamendoim <wesleyvbnardino@hotmail.com>
2023-10-23 16:40:52 -0300

    Ultimo Commit

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ namespace ConsoleApp1
 {
     class Program
     {
-      
+
         static void Main(string[] args)
         {
             Selenium objSelenium = new Selenium();
@@ -20,10 +20,10 @@ namespace ConsoleApp1
             ChromeDriver chromeDriver;
             IWebElement objElement_1;
             IWebElement objElement_2;
-            IWebElement objELement_3;
+            //IWebElement objELement_3;
 
             chromeDriver = objSelenium.BrowserChrome();
-            
+
             chromeDriver.Url = "https://www.google.com.br";
 
             if (chromeDriver.FindElement(By.XPath("html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/textarea")).Displayed)
@@ -33,58 +33,22 @@ namespace ConsoleApp1
                 ///html/body/div[1]/div[3]/form/div[1]/div[1]/div[4]/center/input[1]
                 chromeDriver.FindElement(By.XPath("html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/textarea")).SendKeys(OpenQA.Selenium.Keys.Enter);
 
-                //objElement_1 = chromeDriver.FindElement(By.XPath("//*[@id='tads']/div/div/div/div/div[1]/a/div[1]/span"));
-                //*[@id="tads"]/div/div/div/div/div[2]/a/div[1]/span
+                objElement_1 = chromeDriver.FindElement(By.XPath("//*[@id='Odp5De']/div/div[1]/div/div[1]/block-component/div/div[1]/div/div/div/div/div[1]/div/div/div/div/div[2]/div/div/div[1]/div/span/a/h3"));
+                
+                objElement_2 = chromeDriver.FindElement(By.XPath("//*[@id='rso']/div[2]/div/div/div[1]/div/div/span/a/h3"));
+                
+                Console.WriteLine(objElement_1.Text);
+               
+                Console.WriteLine(objElement_2.Text);
 
-                //objElement_2 = chromeDriver.FindElement(By.XPath("//*[@id='rso']/div[2]/div/div/div[1]/div/div/span/a/h3"));
-
-                ////*[@id="tads"]/div[1]/div/div/div/div[2]/a/div[1]/span
-
-                //Console.WriteLine(objElement_1.Text);
-                //Console.ReadLine();
-
-                //Console.WriteLine(objElement_2.Text);
-                //Console.ReadLine();
-
-
-                //Elemento para realizar um loop e achar os valores caso o ID seja randomico/dinamico
-                objELement_3 = chromeDriver.FindElement(By.XPath("html/body/div[5]/div"));
-
-                List<string> lstDadosPesquisa = new List<string>();
-
-                foreach(string texto in lstDadosPesquisa)
-                {
-                    switch(texto)
-                    {
-                        case "RPA - Automaçao RPA":
-                            Console.WriteLine(texto);
-                            Console.ReadLine();
-                            break;
-
-                        case "Automaçao Processos Robóticos - Automaçao RPA":
-                            Console.WriteLine(texto);
-                            Console.ReadLine();
-                            break;
-
-                        default:
-                            break;
-                    } 
-
-                }
-
-
-                //Automaçao Processos Robóticos - Automaçao RPA
-                //o que é e como emitir o Recibo de Pagamento Autônomo?
-                //RPA - Automaçao RPA
-                //Automaçao Processos Robóticos - Automaçao RPA
-
+                Console.ReadLine();
                 chromeDriver.Quit();
 
             }
             else
             {
                 Console.WriteLine("Elemento não disponivel para o andamento do robô");
-                
+
             }
 
         }
